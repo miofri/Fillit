f60fc3bb0358fee436487d3707f2cd5d9b2d70ec
easternfum <93187444+easternfum@users.noreply.github.com>
2022-03-08 14:17:51 +0200

    Update solution.c
    Updated version 8.3

--- src/solution.c
+++ src/solution.c
@@ -1,5 +1,6 @@
 
 #include "fillit.h"
+#include <time.h>
 
 /*
 **  Round up number to get a square
@@ -9,12 +10,12 @@ static int  round_up_sqrt(int i)
 {
     int size;
 
-    size = 2;
+    size = 1;
     if (i == 1)
     {
         return (1);
     }
-    while (size * size < i)
+    while (size * size + 1 < i)
     {
         size++;
     }
@@ -62,163 +63,89 @@ static int  install_blocks(t_map *map, t_coord *coor, size_t i)
 ** If solution didn't found, free map to avoid memory leak
 */
 
-static void searching_solution(t_map *map, t_blocks *blocks, size_t nb_blocks, size_t *found)
+static void searching_solution(t_map *map, t_blocks *blocks,
+size_t nb_blocks, size_t *found)
 {
     size_t  i;
     size_t  map_size;
-
+    size_t  count;
+    
+    count = 0;
     if (nb_blocks == blocks->nb_blocks)
     {
         *found = 1;
     }
     if (*found == 1)
     {
-        return ;
+        return;
     }
-    i = 0;
+    i = 0;    
     map_size = map->size * map->size;
+    printf("map size: %ld\n", map_size);
     while (i < map_size && !(*found))
     {
         if (install_blocks(map, blocks->blocks[nb_blocks], i))
         {
-            searching_solution(map, blocks, nb_blocks + 1, found);
+            if (count < 22)
+            {
+                searching_solution(map, blocks, nb_blocks + 1, found);
+                count++;
+            }
+            else
+            {
+                return;
+            }
+            if (*found == 0)
+            {
+                free_map(map, blocks->blocks[nb_blocks], i);
+            }
         }
-        if (*found == 0)
-        {
-            free_map(map, blocks->blocks[nb_blocks], i);
-        }
-        i++;
+        ++i;
     }
+    return;
 }
 
-// Eastern's old final_solution code
-
-/*
-** Set block's shape to struct, create map and place blocks
-** Go thru all the possibilities to get the smallest square
-** When the smallest square found, print out map
-** Free block that has been initialized then free map to
-** avoid memory leak
-*/
-
-/*
-int final_solution(char *s)
+static int  output(t_blocks *blocks, size_t map_size)
 {
-    t_map       *map;
-    t_blocks    *blocks;
-    size_t      found;
-    size_t      map_size;
+    t_map   *map;
+    size_t  found;
+    size_t  nb_blocks;
 
     found = 0;
-    blocks = init_blocks(s);
-    if (!blocks)
-    {
-        return (0);
-    }
-    map_size = round_up_sqrt(blocks->nb_blocks) * 2;
-    if (map_size < 4)
-    {
-        map_size = 4;
-    }
+    nb_blocks = 0;
     while (!found)
-    {
-        map = create_map(map_size);
-        if (!map)
+	{
+		map = create_map(map_size);
+		if (!map)
         {
-            return (0);
+			return (0);
         }
-        searching_solution(map, blocks, 0, &found);
-        if(!found)
-        {
-            free(map);
-            map_size++;
-        }
-    }
+		searching_solution(map, blocks, nb_blocks, &found);
+        if (!found)
+		{
+			free(map);
+			map_size++;
+		}
+	}
     print_map(map->new_map, map_size);
     free_block(blocks);
-    free(map);
+	free(map);
     return (1);
 }
-
-*/
-
-// Sheru's old final_solution code
 
 /*
-int	final_solution(char *s, char *argv[])
-{
-    t_map *map;
-	t_blocks	*blocks;
-	size_t		found;
-	size_t		map_size;
-
-	s[0] = 1;
-	found = 0;
-	blocks = parse(argv);
-	if (!blocks)
-		return (0);    
-	map_size = round_up_sqrt(blocks->nb_blocks) * 2;
-	if (map_size < 4)
-		map_size = 4;
-	while (!found)
-	{
-		map = create_map(map_size);
-		if (!map)
-			return (0);
-		searching_solution(map, blocks, 0, &found);
-		if (!found)
-		{
-			free(map);
-			map_size++;
-		}
-	}
-	print_map(map->new_map, map_size);
-    free_block(blocks);
-	free(map);	
-
-	return (1);
-}
+** Searching for the solution to find the smallest square.
 */
-
-
-// New final_solution code
-
-static int  output(t_blocks *blocks, size_t found, size_t map_size)
-{
-    t_map *map;
-
-    while (!found)
-	{
-		map = create_map(map_size);
-		if (!map)
-			return (0);
-		searching_solution(map, blocks, 0, &found);
-		if (!found)
-		{
-			free(map);
-			map_size++;
-		}
-	}
-	print_map(map->new_map, map_size);
-    free_block(blocks);
-	free(map);
-
-    return (1);
-}
 
 int	final_solution(char **s)
 {
 	t_blocks	*blocks;
-	size_t		found;
 	size_t		map_size;
 
-	found = 0;
 	blocks = parse(s);
 	if (!blocks)
 		return (0);    
 	map_size = round_up_sqrt(blocks->nb_blocks) * 2;
-	if (map_size < 4)
-		map_size = 4;
-    output(blocks, found, map_size);
+    output(blocks, map_size);
 	return (1);
 }
